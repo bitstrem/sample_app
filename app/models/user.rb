97997144c3ad296be50b48919caff47c6cb0a19f
Brian Meadows <brian.namaste@gmail.com>
2011-06-04 09:56:29 -0700

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   
   validates :name,  :presence     => true,
                     :length       => { :maximum => 50}
